be413c90f9b3162bf4422f3a789a84d1b356a3a1
dududornelees <eduardo.dsdorneles@gmail.com>
2022-05-05 01:40:52 -0300

    🔥 New: Atualizar carro

--- src/controller/Car.ts
+++ src/controller/Car.ts
@@ -7,7 +7,7 @@ import Car from "../@types/Car";
 
 export const getAllCars = (req: Request, res: Response) => {
     const cars: Car[] = listCars();
-    res.json(cars);
+    res.status(200).json(cars);
 };
 
 export const getCarById = (req: Request, res: Response) => {
@@ -15,10 +15,10 @@ export const getCarById = (req: Request, res: Response) => {
     const foundCar = car.find((car: Car) => car.id === req.params.id);
 
     if (!foundCar) {
-        res.status(404).send("Car not found");
+        res.status(404).json({ msg: "Car not found" });
     }
 
-    res.json(foundCar);
+    res.status(200).json(foundCar);
 };
 
 export const addCar = (req: Request, res: Response) => {
@@ -26,19 +26,19 @@ export const addCar = (req: Request, res: Response) => {
     const cars: Car[] = listCars();
 
     if (!name) {
-        res.status(400).send("Name is required");
+        res.status(400).json({ msg: "Name is required" });
     }
 
     if (!year) {
-        res.status(400).send("Year is required");
+        res.status(400).json({ msg: "Year is required" });
     }
 
     if (!color) {
-        res.status(400).send("Color is required");
+        res.status(400).json({ msg: "Color is required" });
     }
 
     if (!price) {
-        res.status(400).send("Price is required");
+        res.status(400).json({ msg: "Price is required" });
     }
 
     const newCar: Car = {
@@ -51,4 +51,43 @@ export const addCar = (req: Request, res: Response) => {
 
     cars.push(newCar);
     res.json(newCar);
+};
+
+export const updateCar = (req: Request, res: Response) => {
+    const { name, year, color, price } = req.body;
+    const cars: Car[] = listCars();
+
+    let carIndex: number = cars.findIndex(
+        (car: Car) => car.id === req.params.id
+    );
+
+    if (carIndex === -1) {
+        res.status(404).json({ msg: "Car not found" });
+    }
+
+    if (!name) {
+        res.status(400).json({ msg: "Name is required" });
+    }
+
+    if (!year) {
+        res.status(400).json({ msg: "Year is required" });
+    }
+
+    if (!color) {
+        res.status(400).json({ msg: "Color is required" });
+    }
+
+    if (!price) {
+        res.status(400).json({ msg: "Price is required" });
+    }
+
+    cars[carIndex] = {
+        id: req.params.id,
+        name,
+        year,
+        color,
+        price,
+    };
+
+    res.status(200).json(cars[carIndex]);
 };
--- src/routes/Car.ts
+++ src/routes/Car.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
-import { getAllCars, getCarById, addCar } from "../controller/Car";
+import { getAllCars, getCarById, addCar, updateCar } from "../controller/Car";
 
 const carRouter = Router();
 
 carRouter.get("/", getAllCars);
 carRouter.get("/:id", getCarById);
 carRouter.post("/", addCar);
-carRouter.put("/:id");
+carRouter.put("/:id", updateCar);
 carRouter.delete("/:id");
 
 export default carRouter;
